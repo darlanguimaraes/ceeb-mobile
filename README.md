08c6bb695c3ad40e95fec62c7c3f56fa8a7d289a
Darlan Guimaraes <darlanibira@gmail.com>
2023-02-09 10:49:27 -0300

    update icons web, update reademe

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-# ceeb_mobile
+# CEEB Mobile
 
-A new Flutter project.
+Administration for Casa Espírita Euripedes Barsanulpho
 
-## Getting Started
+## Flutter development
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- provider
+- hive database
+- dbcrypt for encrypt password
+- HTTP for synchronize with server
+- build web for PWA
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,35 +28,39 @@ class HomePage extends StatelessWidget {
           } else if (snapshot.error != null) {
             return const Center(child: Text('Ocorreu um erro!'));
           } else {
-            return Container(
-              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
-              width: deviceSize.width > 700 ? 700 : double.infinity,
-              child: Column(
-                children: [
-                  Text(
-                    'Número de livros emprestados: ${snapshot.data!.totalOpenLendings}',
-                    style: const TextStyle(fontSize: 20),
-                  ),
-                  const Divider(),
-                  Text(
-                    'Número de livros em atraso: ${snapshot.data!.totalLateLendings}',
-                    style: const TextStyle(fontSize: 20),
-                  ),
-                  const Divider(),
-                  if (snapshot.data != null)
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: snapshot.data!.lateLendings.length,
-                        itemBuilder: ((ctx, index) => Column(
-                              children: [
-                                LendingListItem(
-                                    snapshot.data!.lateLendings[index], false),
-                                const Divider(),
-                              ],
-                            )),
-                      ),
+            return Center(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+                width: deviceSize.width > 700 ? 700 : double.infinity,
+                child: Column(
+                  children: [
+                    Text(
+                      'Número de livros emprestados: ${snapshot.data!.totalOpenLendings}',
+                      style: const TextStyle(fontSize: 20),
                     ),
-                ],
+                    const Divider(),
+                    Text(
+                      'Número de livros em atraso: ${snapshot.data!.totalLateLendings}',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    const Divider(),
+                    if (snapshot.data != null)
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: snapshot.data!.lateLendings.length,
+                          itemBuilder: ((ctx, index) => Column(
+                                children: [
+                                  LendingListItem(
+                                      snapshot.data!.lateLendings[index],
+                                      false),
+                                  const Divider(),
+                                ],
+                              )),
+                        ),
+                      ),
+                  ],
+                ),
               ),
             );
           }
